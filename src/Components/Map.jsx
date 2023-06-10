5d87b75f80239265cf29c70a52488a6905af08fd
Arc <atandarokib@gmail.com>
2023-06-10 20:52:00 +0100

    up

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Phase1 from "../assets/1.jpg";
+import Phase2 from "../assets/2.jpg";
+import Phase3 from "../assets/3.jpg";
 
 const Map = () => {
-  return <div>Map</div>;
+  return (
+    <div className="py-[50px] w-full Mics">
+      <div className="container  flex flex-col gap-14 justify-center text-center items-center">
+        <h1 className="text-4xl decoration-wavy underline-offset-[20px] overline uppercase font-bold">
+          roadmap
+        </h1>
+
+        <div className="w-full grid-cols-1  lg grid md:grid-cols-3 gap-8 ">
+          <img src={Phase1} alt="" />
+          <img src={Phase2} alt="" />
+          <img src={Phase3} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Map;

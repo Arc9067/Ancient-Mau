a55fb2917f41e867d1ddb14aa1032cbf396ef6f2
Arc <atandarokib@gmail.com>
2023-06-10 21:26:52 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     const event = window.addEventListener("load", () => {
-      setTimeout(() => setLoading(false), 3000);
+      setTimeout(() => setLoading(false), 1000);
     });
 
     // gsap.defaults({ duration: 1 });
